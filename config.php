9fc65a6c7515298c7d9bb73e71001a9173809ef1
yasmin mohamed <myasmin91@yahoo.com>
2021-09-20 21:25:57 +0200

    edit task
    edit task

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+define('BASE_URL','https://localhost/ME/task3EDIT/blog/site');
+define('BASE_PATH', __DIR__);
---
+++ myposts/edit.php
@@ -0,0 +1,111 @@
+<?php
+require_once('../config.php');
+require_once(BASE_PATH . '/logic/posts.php');
+require_once(BASE_PATH . '/logic/tags.php');
+require_once(BASE_PATH . '/logic/categories.php');
+$post_id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
+
+function getUserId()
+{
+    if (session_status() != PHP_SESSION_ACTIVE) session_start();
+    if (isset($_SESSION['user'])) return $_SESSION['user']['id'];
+    return 0;
+}
+$tags = getTags();
+$categories = getCategories();
+$old_post_data=getOldPostData(getUserId(),$post_id);
+
+if (isset($_REQUEST['title'])) {
+    $errors = validatePostCreate($_REQUEST);
+    if (count($errors) == 0) {
+
+        if (updatePost($_REQUEST,  $post_id , getUploadedImage($_FILES))) {
+            header('Location:index.php');
+            die();
+        } else {
+            $generic_error = "Error while adding the post";
+        }
+    }
+}
+
+require_once(BASE_PATH . '/layout/header.php');
+?>
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="heading-page header-text">
+    <section class="page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-content">
+                        <h4>Add Post</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!-- Banner Ends Here -->
+<section class="blog-posts">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="all-blog-posts">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <form method="POST" enctype="multipart/form-data">
+                                <input name="title" value="<?php echo $old_post_data['title']; ?>" type="text"
+                                    placeholder="title" class="form-control" />
+                                <?= isset($errors['title']) ? "<span class='text-danger'>" . $errors['title'] . "</span>" : "" ?>
+                                <textarea name=" content" value="<?php echo $old_post_data['content']; ?>"
+                                    placeholder="content" class="form-control"></textarea>
+                                <?= isset($errors['content']) ? "<span class='text-danger'>" . $errors['content'] . "</span>" : "" ?>
+                                <label>Upload Image<input type="file"
+                                        value="<?php echo BASE_URL.$post['image'].$old_post_data['image']; ?>"
+                                        name="image" /></label><br />
+                                <?= isset($errors['image']) ? "<span class='text-danger'>" . $errors['image'] . "</span>" : "" ?>
+                                <label>Publish date<input type="date"
+                                        value="<?php echo date('Y-m-d',strtotime($old_post_data['publish_date'])); ?>"
+                                        name="publish_date" class="form-control"></label>
+                                <?= isset($errors['publish_date']) ? "<span class='text-danger'>" . $errors['publish_date'] . "</span>" : "" ?>
+                                <select name="category_id" class="form-control" value="">
+                                    <option value="">Select category</option>
+                                    <?php
+                                    foreach ($categories as $category) {
+                                        echo "<option " . ($category['id'] == $old_post_data['category_id'] ? "selected" : "") . " value='{$category['id']}'>{$category['name']}</option>";
+                                    }
+                                    ?>
+                                </select>
+                                <?= isset($errors['category_id']) ? "<span class='text-danger'>" . $errors['category_id'] . "</span>" : "" ?>
+                                <select name="tags[]" multiple class="form-control"
+                                    value="<?php echo $old_post_data['tags']; ?>">
+                                    <?php
+                                    foreach ($tags as $tag)
+                                    {
+                                        if (count($tags) > 0)
+                                        {
+                                            echo "<option " . (($tags['id']== $old_post_data['tags']['id']) ? "selected" : "") . " value='{$tag['id']}'>{$tag['name']}</option>";
+                                        }
+                                        else
+                                        {
+                                            echo "<option value='{$tag[' id']}'>{$tag['name']}</option>";
+
+                                        }
+                                    }
+                                ?>
+                                </select>
+                                <?= isset($errors['tags']) ? "<span class='text-danger'>" . $errors['tags'] . "</span>" : "" ?>
+                                <button class="btn btn-success">Update Post</button>
+                                <a href="index.php" class="btn btn-danger">Cancel</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require_once(BASE_PATH . '/layout/footer.php') ?>
